60341f39bf28834e71da2e8c0837cb48921a295e
harshetmaruff <harshet.maruff@gmail.com>
2020-11-03 21:37:34 +0530

    remade the project because i messed up with the fps thing

---
+++ Charcters/Scripts/Player.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+
+var motion = Vector2()
+var hp = 50 
+var Character
+var sprite
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func move_it():
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
