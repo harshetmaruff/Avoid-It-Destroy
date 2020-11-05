5921b8f8dbec4dad37768b7f175711bde1b478d5
harshetmaruff <harshet.maruff@gmail.com>
2020-11-05 23:32:24 +0530

    Added power to read json files

--- Scripts/Scene2.gd
+++ Scripts/Scene2.gd
@@ -15,8 +15,17 @@ func _ready():
 	var dialogbars = dialogbar.instance()
 	dialogbars.set_position(dialogposition)
 	add_child(dialogbars)
+	var jsjsj = get_json_as_text("Dialogs/OpeningScene2.json")
+	print(jsjsj)
 
-
+func get_json_as_text(filename):
+	var file = File.new()
+	file.open(filename, File.READ)
+	var text = file.get_as_text()
+	var data = parse_json(text)
+	file.close()
+	print(text)
+	return data
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
