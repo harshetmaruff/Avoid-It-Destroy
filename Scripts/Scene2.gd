d2696def9d48956cc855da78486ae3fa690e8969
harshetmaruff <harshet.maruff@gmail.com>
2020-11-06 18:41:45 +0530

    edit scene for dialog system

--- Scripts/Scene2.gd
+++ Scripts/Scene2.gd
@@ -6,7 +6,8 @@ extends Node2D
 # var b = "text"
 var dialogposition = Vector2(1024/2, 460)
 export (PackedScene) var dialogbar
-
+var dialognumber = 0
+var jsjsj
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,8 +16,9 @@ func _ready():
 	var dialogbars = dialogbar.instance()
 	dialogbars.set_position(dialogposition)
 	add_child(dialogbars)
-	var jsjsj = get_json_as_text("Dialogs/OpeningScene2.json")
-	print(jsjsj)
+	jsjsj = get_json_as_text("Dialogs/OpeningScene2.json")
+	get_tree().call_group("DialogSystem", "displaydialog", jsjsj, dialognumber)
+	set_process(true)
 
 func get_json_as_text(filename):
 	var file = File.new()
@@ -24,8 +26,10 @@ func get_json_as_text(filename):
 	var text = file.get_as_text()
 	var data = parse_json(text)
 	file.close()
-	print(text)
 	return data
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		if not dialognumber == len(jsjsj) - 1:
+			dialognumber += 1
+			get_tree().call_group("DialogSystem", "displaydialog", jsjsj, dialognumber)
