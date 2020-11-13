46555e9918c0412c9c0858c98ad6c9780f017c5a
harshetmaruff <harshet.maruff@gmail.com>
2020-11-13 15:18:06 +0530

    edition of facingdirection

--- Charcters/Scripts/Player.gd
+++ Charcters/Scripts/Player.gd
@@ -6,7 +6,9 @@ var facingdirection = Vector2()
 # 22/1/2021
 var hp = 50 
 var Character
-var sprite
+onready var sprite = $AnimatedSprite
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process(true)
@@ -15,8 +17,22 @@ func _ready():
 	print(facingdirection)
 
 func move_it(direction):
+	if direction == "Up":
+		facingdirection.y = -1
+		facingdirection.x = 0
+	if direction == "Down":
+		facingdirection.y = 1
+		facingdirection.x = 0
+	if direction == "Left":
+		facingdirection.y = 0
+		facingdirection.x = -1
+	if direction == "Right":
+		facingdirection.y = 0
+		facingdirection.x = 1
 	print(direction)
+	print(facingdirection)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	var the_current_pos = get_position()
 	if Input.is_action_pressed("PlayerUp") and not Input.is_action_pressed("PlayerDown"):
